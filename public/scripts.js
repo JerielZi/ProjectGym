a37b9d84497d4f9368a784156184885b272c554c
JerielZi <jeriel_zinga@hotmail.com>
2020-05-05 15:58:52 +0100

    Setting the option create in both pages

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,11 @@
 const currentPage = location.pathname
+const menuItems = document.querySelectorAll("header .links a")
 
-console.log(currentPage)
+for(item of menuItems) {
+  if (currentPage.includes(item.getAttribute("href"))) {
+    item.classList.add("active")
+  }
+}
 
 
 
